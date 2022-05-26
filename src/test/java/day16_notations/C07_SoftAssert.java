63999630239fe8fbf32acb1363858a89365ac21d
Cevdet Gulser <c.gulser86@gmail.com>
2022-05-26 22:25:45 +0200

    26.05.2022

---
+++ src/test/java/day16_notations/C07_SoftAssert.java
@@ -0,0 +1,29 @@
+package day16_notations;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C07_SoftAssert extends TestBase {
+    @Test
+    public void test01() throws InterruptedException {
+        //1. “http://zero.webappsecurity.com/” Adresine gidin
+        driver.get("http://zero.webappsecurity.com");
+        //2. Sign in butonuna basin
+
+        driver.findElement(By.id("signin_button")).click();
+        Thread.sleep(5000);
+        //3. Login kutusuna “username” yazin
+
+        //4. Password kutusuna “password” yazin
+        //5. Sign in tusuna basin
+        //6. Online banking menusu icinde Pay Bills sayfasina gidin
+        //7. “Purchase Foreign Currency” tusuna basin
+        //8. “Currency” drop down menusunden Eurozone’u secin
+        //9. soft assert kullanarak "Eurozone (Euro)" secildigini test edin
+        //10. soft assert kullanarak DropDown listesinin su secenekleri oldugunu test edin "Select One", "Australia (dollar)", "Canada (dollar)","Switzerland (franc)","China (yuan)","Denmark (krone)","Eurozone (euro)","Great Britain (pound)","Hong Kong (dollar)","Japan (yen)","Mexico (peso)","Norway (krone)","New Zealand (dollar)","Sweden (krona)","Singapore (dollar)","Thailand (baht)"
+
+
+    }
+}
